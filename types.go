c4809d581b164f09d58cfd312b13e2ec7ecca91e
Hữu Hà <business@zsmart.tech>
2021-08-11 18:59:23 +0000

    add MatchingPayloadMessage

--- types.go
+++ types.go
@@ -1,5 +1,7 @@
 package pkg
 
+import "github.com/zsmartex/pkg/order"
+
 type PayloadAction = string
 
 var (
@@ -8,3 +10,10 @@ var (
 	ActionCancelWithKey PayloadAction = "cancel_with_key" // this method will not notify to the user
 	ActionReload        PayloadAction = "reload"
 )
+
+type MatchingPayloadMessage struct {
+	Action PayloadAction   `json:"action"`
+	Order  *order.Order    `json:"order"`
+	Key    *order.OrderKey `json:"key"`
+	Market string          `json:"market"`
+}
