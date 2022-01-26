9e65f13dbfcd029b5e45753c3d79690c3c386837
Hữu Hà <business@zsmart.tech>
2022-01-27 00:02:45 +0700

    Update fetch proto

--- proto/Grpc/engine/utils.go
+++ proto/Grpc/engine/utils.go
@@ -1,7 +1,9 @@
 package GrpcEngine
 
 import (
+	"github.com/google/uuid"
 	"github.com/shopspring/decimal"
+	"github.com/zsmartex/pkg/order"
 )
 
 func (d *Decimal) ToDecimal() decimal.Decimal {
@@ -10,4 +12,39 @@ func (d *Decimal) ToDecimal() decimal.Decimal {
 
 func (d *Decimal) ToNullDecimal() decimal.NullDecimal {
 	return decimal.NewNullDecimal(d.ToDecimal())
+}
+
+func (r *FetchOrderRequest) ToOrderKey() *order.OrderKey {
+	uuid, _ := uuid.FromBytes(r.Uuid)
+
+	return &order.OrderKey{
+		ID:        r.Id,
+		UUID:      uuid,
+		Symbol:    r.Symbol,
+		Side:      order.OrderSide(r.Side),
+		Price:     r.Price.ToDecimal(),
+		StopPrice: r.StopPrice.ToDecimal(),
+		Fake:      r.Fake,
+		CreatedAt: r.CreatedAt.AsTime(),
+	}
+}
+
+func (r *FetchOrderResponse) ToOrder() *order.Order {
+	uuid, _ := uuid.FromBytes(r.Uuid)
+
+	return &order.Order{
+		ID:             r.Id,
+		UUID:           uuid,
+		Symbol:         r.Symbol,
+		MemberID:       r.MemberId,
+		Side:           order.OrderSide(r.Side),
+		Type:           order.OrderType(r.Type),
+		Price:          r.Price.ToDecimal(),
+		StopPrice:      r.StopPrice.ToDecimal(),
+		Quantity:       r.Quantity.ToDecimal(),
+		FilledQuantity: r.FilledQuantity.ToDecimal(),
+		Cancelled:      r.Cancelled,
+		Fake:           r.Fake,
+		CreatedAt:      r.CreatedAt.AsTime(),
+	}
 }
