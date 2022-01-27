9aef199add54ad7a70d29e4d4eba05957db2fd5a
Hữu Hà <business@zsmart.tech>
2022-01-28 00:30:45 +0700

    Update quantex proto

---
+++ client/quantex/quantex.go
@@ -0,0 +1,33 @@
+package clientQuantex
+
+import (
+	"context"
+	"log"
+	"os"
+
+	GrpcQuantex "github.com/zsmartex/pkg/Grpc/quantex"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type GrpcQuantexClient struct {
+	connect *grpc.ClientConn
+	client  GrpcQuantex.QuantexServiceClient
+}
+
+func NewQuantexClient() *GrpcQuantexClient {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(os.Getenv("QUANTEX_ENGINE_URL"), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+
+	return &GrpcQuantexClient{
+		connect: conn,
+		client:  GrpcQuantex.NewQuantexServiceClient(conn),
+	}
+}
+
+func (c *GrpcQuantexClient) UpdateOrder(in *GrpcQuantex.UpdateOrderRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	return c.client.UpdateOrder(context.Background(), in, opts...)
+}
--- scripts/gen.sh
+++ scripts/gen.sh
@@ -4,3 +4,5 @@ protoc -I=$PWD --go_out=$GOPATH/src/ $PWD/proto/utils.proto
 protoc -I=$PWD --go_out=$GOPATH/src/ $PWD/proto/order.proto
 protoc -I=$PWD --go_out=$GOPATH/src/ $PWD/proto/engine.proto
 protoc -I=$PWD --go-grpc_opt=require_unimplemented_servers=false --go-grpc_out=$GOPATH/src/ $PWD/proto/engine.proto
+protoc -I=$PWD --go_out=$GOPATH/src/ $PWD/proto/quantex.proto
+protoc -I=$PWD --go-grpc_opt=require_unimplemented_servers=false --go-grpc_out=$GOPATH/src/ $PWD/proto/quantex.proto
