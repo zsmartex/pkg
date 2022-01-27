a095b6d8b924a5351d4ad8cbbf31f2a93038ca2b
Hữu Hà <business@zsmart.tech>
2022-01-27 16:06:46 +0700

    Update

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,2 +1,6 @@
-protoc -I=$PWD --go-grpc_opt=require_unimplemented_servers=false --go-grpc_out=$PWD $PWD/proto/engine/*
-protoc -I=$PWD --go_out=$PWD $PWD/proto/engine/*
+protoc -I=proto --go_out=./gen proto/utils.proto
+
+protoc --go-grpc_opt=require_unimplemented_servers=false --go-grpc_out=$PWD $PWD/proto/engine.proto
+protoc --go_out=$PWD $PWD/proto/engine.proto
+protoc -I=$PWD --go-grpc_opt=require_unimplemented_servers=false --go-grpc_out=$GOPATH/src/ $PWD/proto/*.proto
+protoc -I=$PWD --go-grpc_out=$GOPATH/src/ $PWD/proto/*.proto
