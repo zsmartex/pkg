1661047b0a56a0feb92499e22ae3ed3211f3ab63
Hữu Hà <business@zsmart.tech>
2022-02-01 17:42:10 +0700

    Add support kafka multiple topic subscription

--- services/kafka.go
+++ services/kafka.go
@@ -8,44 +8,61 @@ import (
 )
 
 type KafkaClient struct {
-	consumer *kafka.Consumer
-	producer *kafka.Producer
+	Consumer *kafka.Consumer
+	Producer *kafka.Producer
 }
 
 func NewKafka() *KafkaClient {
 	return &KafkaClient{}
 }
 
+func (k *KafkaClient) CreateConsumer() (*kafka.Consumer, error) {
+	return kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  os.Getenv("KAFKA_URL"),
+		"enable.auto.commit": false,
+		"group.id":           os.Getenv("KAFKA_GROUP_ID"),
+	})
+}
+
 func (k *KafkaClient) Subscribe(topic string, callback func([]byte) error) {
-	if k.consumer == nil {
-		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-			"bootstrap.servers":  os.Getenv("KAFKA_URL"),
-			"enable.auto.commit": false,
-			"group.id":           os.Getenv("KAFKA_GROUP_ID"),
-		})
+	if k.Consumer == nil {
+		consumer, err := k.CreateConsumer()
 		if err != nil {
 			panic("Can't create consumer due to error: " + err.Error())
 		}
 
-		k.consumer = consumer
+		k.Consumer = consumer
 	}
 
-	k.consumer.SubscribeTopics([]string{topic}, nil)
+	k.SubscribeTopics([]string{topic}, nil)
 
 	for {
-		msg, err := k.consumer.ReadMessage(-1)
+		msg, err := k.Consumer.ReadMessage(-1)
 		if err != nil {
 			log.Printf("Consumer error: %v (%v)\n", err, msg)
 		}
 
 		if err := callback(msg.Value); err == nil {
-			k.consumer.Commit()
+			k.Consumer.CommitMessage(msg)
 		}
 	}
 }
 
+func (k *KafkaClient) SubscribeTopics(topics []string, rebalanceCb kafka.RebalanceCb) error {
+	if k.Consumer == nil {
+		consumer, err := k.CreateConsumer()
+		if err != nil {
+			panic("Can't create consumer due to error: " + err.Error())
+		}
+
+		k.Consumer = consumer
+	}
+
+	return k.Consumer.SubscribeTopics(topics, rebalanceCb)
+}
+
 func (k *KafkaClient) Publish(topic string, body []byte) error {
-	if k.producer == nil {
+	if k.Producer == nil {
 		producer, err := kafka.NewProducer(&kafka.ConfigMap{
 			"bootstrap.servers": os.Getenv("KAFKA_URL"),
 		})
@@ -53,10 +70,12 @@ func (k *KafkaClient) Publish(topic string, body []byte) error {
 			panic("Can't create producer due to error: " + err.Error())
 		}
 
-		k.producer = producer
+		k.Producer = producer
+
+		k.Producer.GetFatalError()
 	}
 
-	err := k.producer.Produce(&kafka.Message{
+	err := k.Producer.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Value:          body,
 	}, nil)
@@ -65,7 +84,7 @@ func (k *KafkaClient) Publish(topic string, body []byte) error {
 		return err
 	}
 
-	k.producer.Flush(100)
+	k.Producer.Flush(100)
 
 	return nil
 }
