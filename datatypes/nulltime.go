13566a188dc765125d1a886243bc5444e2140f5a
Hữu Hà <huuhadz2k@gmail.com>
2022-03-16 13:06:50 +0700

    Add support jwt, decimal, datatypes with bson to pkg

---
+++ datatypes/nulltime.go
@@ -0,0 +1,84 @@
+package datatypes
+
+import (
+	"database/sql"
+	"encoding/json"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
+)
+
+type NullTime struct {
+	sql.NullTime
+}
+
+// IsZero method is called by bson.IsZero in Mongo for type = NullTime
+func (x NullTime) IsZero() bool {
+	return !x.Valid
+}
+
+// MarshalBSONValue method is called by bson.Marshal in Mongo for type = NullTime
+func (x *NullTime) MarshalBSONValue() (bsontype.Type, []byte, error) {
+	if !x.Valid {
+		return bsontype.Null, nil, nil
+	}
+
+	valueType, b, err := bson.MarshalValue(x.Time)
+	return valueType, b, err
+}
+
+// UnmarshalBSONValue method is called by bson.Unmarshal in Mongo for type = NullTime
+func (x *NullTime) UnmarshalBSONValue(t bsontype.Type, data []byte) error {
+	raw := bson.RawValue{Type: t, Value: data}
+	uTime := time.Time{}
+
+	if err := raw.Unmarshal(&uTime); err != nil {
+		return err
+	}
+
+	if raw.Value == nil {
+		x.Valid = false
+		return nil
+	}
+
+	x.Valid = true
+	x.Time = uTime
+	return nil
+}
+
+// MarshalJSON method is called by the JSON marshaller
+func (x *NullTime) MarshalJSON() ([]byte, error) {
+	if !x.Valid {
+		return []byte("null"), nil
+	}
+
+	b, err := json.Marshal(x.Time)
+	return b, err
+}
+
+// UnmarshalJSON method is called by the JSON unmarshaller
+func (x *NullTime) UnmarshalJSON(data []byte) error {
+	x.Valid = false
+
+	if data == nil {
+		return nil
+	}
+
+	var timeString string
+	if err := json.Unmarshal(data, &timeString); err != nil {
+		return err
+	}
+	if timeString == "" {
+		return nil
+	}
+
+	uTime, err := time.Parse(time.RFC3339, timeString)
+	if err != nil {
+		return err
+	}
+
+	x.Valid = true
+	x.Time = uTime
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/zsmartex/pkg
 go 1.16
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/protobuf v1.5.2
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/uuid v1.3.0
@@ -11,10 +12,12 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/twmb/franz-go v1.3.1
 	github.com/twmb/franz-go/pkg/kadm v0.0.0-20220128010747-0a23cca3f4ee
+	go.mongodb.org/mongo-driver v1.8.4
 	golang.org/x/crypto v0.0.0-20220128200615-198e4374d7ed // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	google.golang.org/grpc v1.44.0
 	google.golang.org/protobuf v1.27.1
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 	gorm.io/driver/postgres v1.2.3
 	gorm.io/gorm v1.22.5
 )
