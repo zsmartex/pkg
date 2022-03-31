8b5704f6b83ef5ca90f0d4241778ae1b5012694b
Hữu Hà <huuhadz2k@gmail.com>
2022-03-31 10:08:55 +0700

    Update db logger

--- services/database.go
+++ services/database.go
@@ -1,15 +1,55 @@
 package services
 
 import (
+	"context"
+	"errors"
 	"fmt"
-	"log"
-	"os"
 	"time"
 
+	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
+
+type DBlogger struct {
+	SlowThreshold         time.Duration
+	SkipErrRecordNotFound bool
+}
+
+func (d *DBlogger) LogMode(logger.LogLevel) logger.Interface {
+	return d
+}
+
+func (d *DBlogger) Info(ctx context.Context, s string, args ...interface{}) {
+	log.WithContext(ctx).Infof(s, args)
+}
+
+func (d *DBlogger) Warn(ctx context.Context, s string, args ...interface{}) {
+	log.WithContext(ctx).Warnf(s, args)
+}
+
+func (d *DBlogger) Error(ctx context.Context, s string, args ...interface{}) {
+	log.WithContext(ctx).Errorf(s, args)
+}
+
+func (d *DBlogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	elapsed := time.Since(begin)
+	sql, _ := fc()
+	fields := log.Fields{}
+	if err != nil && !(errors.Is(err, gorm.ErrRecordNotFound) && d.SkipErrRecordNotFound) {
+		fields[log.ErrorKey] = err
+		log.WithContext(ctx).WithFields(fields).Errorf("%s [%s]", sql, elapsed)
+		return
+	}
+
+	if d.SlowThreshold != 0 && elapsed > d.SlowThreshold {
+		log.WithContext(ctx).WithFields(fields).Warnf("%s [%s]", sql, elapsed)
+		return
+	}
+
+	log.WithContext(ctx).WithFields(fields).Debugf("%s [%s]", sql, elapsed)
+}
 
 func NewDatabase(host string, port int, user, password, dbname string) (*gorm.DB, error) {
 	var dialector gorm.Dialector
@@ -18,19 +58,12 @@ func NewDatabase(host string, port int, user, password, dbname string) (*gorm.DB
 
 	dialector = postgres.Open(dsn)
 
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold:             time.Second,   // Slow SQL threshold
-			LogLevel:                  logger.Silent, // Log level
-			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
-			Colorful:                  false,         // Disable color
-		},
-	)
-
 	db, err := gorm.Open(dialector, &gorm.Config{
 		SkipDefaultTransaction: true,
-		Logger:                 newLogger,
+		Logger: &DBlogger{
+			SlowThreshold:         200 * time.Millisecond,
+			SkipErrRecordNotFound: true,
+		},
 	})
 
 	if err != nil {
