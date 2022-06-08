371b7a0a395f9b82ba2bb6fff8e8abfdc8946de5
Hữu Hà <huuhadz2k@gmail.com>
2022-06-08 13:08:33 +0700

    Fix vault transit missing (#23)

--- services/transit.go
+++ services/transit.go
@@ -8,29 +8,31 @@ import (
 )
 
 type TransitService struct {
-	vault_service *VaultService
+	vault_service          *VaultService
+	vault_application_name string
 }
 
-func NewTransitService(vault_service *VaultService) *TransitService {
+func NewTransitService(vault_service *VaultService, vault_application_name string) *TransitService {
 	return &TransitService{
-		vault_service: vault_service,
+		vault_service:          vault_service,
+		vault_application_name: vault_application_name,
 	}
 }
 
 func (s *TransitService) Encrypt(key, value string) (*api.Secret, error) {
 	valueString := base64.StdEncoding.EncodeToString([]byte(value))
 
-	return s.vault_service.Write(fmt.Sprintf("transit/encrypt/%s", key), map[string]interface{}{
+	return s.vault_service.Write(fmt.Sprintf("transit/encrypt/%s_%s", s.vault_application_name, key), map[string]interface{}{
 		"plaintext": valueString,
 	})
 }
 
 func (s *TransitService) Decrypt(key, ciphertext string) (*api.Secret, error) {
-	return s.vault_service.Write(fmt.Sprintf("transit/encrypt/%s", key), map[string]interface{}{
+	return s.vault_service.Write(fmt.Sprintf("transit/encrypt/%s_%s", s.vault_application_name, key), map[string]interface{}{
 		"ciphertext": ciphertext,
 	})
 }
 
 func (s *TransitService) Delete(key string) (*api.Secret, error) {
-	return s.vault_service.Delete(fmt.Sprintf("transit/encrypt/%s", key))
+	return s.vault_service.Delete(fmt.Sprintf("transit/encrypt/%s_%s", s.vault_application_name, key))
 }
