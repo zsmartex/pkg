07b29932dd9cb6f85fe76d584d9495f3dae9c492
huuhait <huuhadz2k@gmail.com>
2024-02-18 07:30:47 +0100

    add-support-config-redis

--- infrastructure/fiber_fx/middleware/limiter/limiter.go
+++ infrastructure/fiber_fx/middleware/limiter/limiter.go
@@ -6,10 +6,17 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/storage/redis/v3"
+	"github.com/zsmartex/pkg/v2/config"
 )
 
-func New() func(*fiber.Ctx) error {
-	store := redis.New()
+func New(config config.Redis) func(*fiber.Ctx) error {
+	store := redis.New(redis.Config{
+		Host:     config.Host,
+		Port:     config.Port,
+		Password: config.Password,
+		Database: 0,
+	})
+
 	return limiter.New(limiter.Config{
 		Max:        10,
 		Expiration: 1 * time.Minute,
